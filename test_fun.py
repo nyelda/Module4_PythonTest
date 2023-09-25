35cdadfb0dceb56fc30f5175bf4c8df4a9153601
NetAcad DEVASC <changeme@example.com>
2023-09-25 04:45:06 +0000

    .py codes

---
+++ test_fun.py
@@ -0,0 +1,16 @@
+import unittest
+
+def fun(x):
+
+    return x+1
+
+
+class MyTest(unittest.TestCase):
+
+        def test(self):
+            self.assertEqual(fun(5),6)
+
+        
+if __name__== '__main__':
+
+    unittest.main()
